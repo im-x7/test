dad0c7adf2377ab0362731f69ef42c8503c92a50
imx7 <imx7@example.com>
2025-03-21 15:03:48 +0800

    style(component): 格式化测试组件

--- src/components/test.vue
+++ src/components/test.vue
@@ -1,19 +1,19 @@
 <template>
   <div class="test">
-    <h1>   测试组件    </h1>
+    <h1>测试组件</h1>
     <div>
-      {{ message}}
+      {{ message }}
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-const message = "Hello World"
+const message = 'Hello World'
 </script>
 
 <style scoped>
-.test{
-  color:red;
-  padding:10px
+.test {
+  color: red;
+  padding: 10px;
 }
-</style> 
+</style>
