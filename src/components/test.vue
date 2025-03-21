94a19bbadd1056e7f8e0ba4275c4dcaf2fd8c228
imx7 <imx7@example.com>
2025-03-21 14:59:50 +0800

    test(component): 添加测试组件

---
+++ src/components/test.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="test">
+    <h1>   测试组件    </h1>
+    <div>
+      {{ message}}
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+const message = "Hello World"
+</script>
+
+<style scoped>
+.test{
+  color:red;
+  padding:10px
+}
+</style> 
